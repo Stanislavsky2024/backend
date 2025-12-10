92b9908a01dcfaa7b925618af68efbefa492a83d
Stanislavsky2024 <filepark.stas@gmail.com>
2025-12-10 15:16:36 +0900

    initialize

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(configService: ConfigService) {
+    const url = configService.getOrThrow<string>('DATABASE_URL');
+    const adapter = new PrismaPg({ connectionString: url });
+    super({ adapter });
+  }
+}
